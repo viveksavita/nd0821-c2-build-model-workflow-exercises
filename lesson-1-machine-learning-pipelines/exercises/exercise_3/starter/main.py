2a912ee7424569a89de0967fe581cd3e846ef3fc
Vivek savita <viveksavita@192.168.1.103>
2022-04-21 14:35:02 +0100

    Developing of mlops workflows and setting up configuration using hydra

--- lesson-1-machine-learning-pipelines/exercises/exercise_3/starter/main.py
+++ lesson-1-machine-learning-pipelines/exercises/exercise_3/starter/main.py
@@ -27,6 +27,17 @@ def go(config: DictConfig):
     )
 
     ##################
+
+    _=mlflow.run(
+        uri=os.path.join(root_path, "process_data"),
+        entry_point= "main",
+        parameters={
+        "input_artifact": "iris.csv:latest",
+        "artifact_name": "cleaned_data.csv",
+        "artifact_type": "cleaned_data",
+        "artifact_description": "processed data"
+        },
+        )
     # Your code here: use the artifact we created in the previous step as input for the `process_data` step
     # and produce a new artifact called "cleaned_data".
     # NOTE: use os.path.join(root_path, "process_data") to get the path
